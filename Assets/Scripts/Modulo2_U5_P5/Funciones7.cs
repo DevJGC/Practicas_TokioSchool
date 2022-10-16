b52a618ef45342894dfef19e1255d084f8e7018a
Jose Garcia <jgcordoba@gmail.com>
2022-10-16 20:08:38 +0200

    Nuevo Ejercicio Funciones

--- Assets/Scripts/Modulo2_U5_P5/Funciones7.cs
+++ Assets/Scripts/Modulo2_U5_P5/Funciones7.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Funciones7 : MonoBehaviour
 {
-   
+
+    // Ejercicio 7 - Busca coincidencias de nombres en un Array
+    public string[] nombres; // Array con 10 nombres de vecinos   
+    public int v; // Contador de coincidencias de nombres
+
     void Start()
     {
-        
+        // Llama a la función y muestra el valor devuelto
+        BuscaNombres("Jose");
+        Debug.Log(v);
     }
 
-   
-    void Update()
-    {
-        
+
+    // Recorre con un bucle todas las posiciones del Array buscando un nombre, cuando lo encuentra suma 1 a v  
+    public int BuscaNombres(string nombre) 
+    {  
+        for (int i = 0; i < nombres.Length; i++)       
+        {
+            if (nombres[i] == nombre)            
+            {
+                v++;         
+            }       
+        }
+
+        return v;
     }
 }
