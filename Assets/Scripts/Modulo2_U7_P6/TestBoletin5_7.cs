5ccdcbf2383d0f9f65fd2c1faab7291545e7f336
JoseGarciaCordoba <jgcordoba@gmail.com>
2022-10-29 19:37:57 +0200

    Nuevo Script M2 U7

---
+++ Assets/Scripts/Modulo2_U7_P6/TestBoletin5_7.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Ejercicio 7 - Al quitar ": MonoBehaviour" ya no permite asociar el Script a ningún GameObject, ya que no hereda de esta clase principal de Unity
+public class TestBoletin5_7 
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
